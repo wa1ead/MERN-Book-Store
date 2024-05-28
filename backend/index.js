006dd347752f293337b84ea56e6682f7ed741fc0
wa1ead <waliddraa2@gmail.com>
2024-05-28 16:55:09 +0100

    mongoose: Save new Book

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,43 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDbUrl } from "./config.js";
+import { Book } from "./models/BookModel.js";
 
 const app = express();
+
+//Middelware for parsing request
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome To MERN Stack World");
+});
+
+//Route for adding a Book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return respone
+        .status(400)
+        .send({ message: "Please fill in all the fields" });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    respone.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
