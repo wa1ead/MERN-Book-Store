53220e3a74322ecd5527c117a2d3b3c924e71193
wa1ead <waliddraa2@gmail.com>
2024-05-30 16:20:17 +0100

    express: CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDbUrl } from "./config.js";
 import booksRoute from "./routes/booksRoute.js";
 
