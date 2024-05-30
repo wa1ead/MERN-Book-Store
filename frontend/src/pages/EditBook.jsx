ac7a5b449a4282d5e900c805dce715f165cd4545
wa1ead <waliddraa2@gmail.com>
2024-05-30 16:46:09 +0100

    react-router-dom: SPA routing

---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditBook() {
+  return <div>EditBook</div>;
+}
---
+++ frontend/src/pages/ShowBooks.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ShowBooks() {
+  return <div>ShowBook</div>;
+}
